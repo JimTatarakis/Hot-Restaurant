7097238128afb5f786d878c26836cebc5c091315
LiamH <liamhearts@gmail.com>
2019-06-06 19:32:33 -0700

    set-up basics of node server

---
+++ app.js
@@ -0,0 +1,57 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var reservations = [
+    {
+        name: "Yoda",
+        phone: 9999999999,
+        email: "email@emai.com",
+        ID: 1234
+    },
+    {
+        name: "Liam",
+        phone: 8888888888,
+        email: "liam@emai.com",
+        ID: 5678
+    },
+    {
+        name: "Jim",
+        phone: 7777777777,
+        email: "jim@emai.com",
+        ID: 8765
+    },
+    {
+        name: "Alex",
+        phone: 6666666666,
+        email: "alex@emai.com",
+        ID: 4321
+    },
+];
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "html/home.html"));
+});
+
+app.get("/view", function (req, res) {
+    res.sendFile(path.join(__dirname, "html/tables.html"));
+});
+
+app.get("/make", function (req, res) {
+    res.sendFile(path.join(__dirname, "html/reservation.html"));
+});
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
